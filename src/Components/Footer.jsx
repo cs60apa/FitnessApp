242ff3f6e292c530d1301e1c154786e4719413b9
marksikaundi <emmanuelmark117@gmail.com>
2023-03-17 23:52:02 +0200

    updates

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -10,7 +10,7 @@ function Footer() {
   return (
     <div className="max-w[1240px] mx-auto py-16 px-4 grid lg:grid-cols-3 gap-8 text-gray-300 ">
       <div>
-        <h1 className="w-full text-3xl font-bold text-[#00df9a]">StartHub</h1>
+        <h1 className="w-full text-3xl font-bold text-[#00df9a]">Otofast</h1>
         <p className="py-4">
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident,
           velit, ea dolor mollitia id, expedita animi repellendus ducimus eos
@@ -62,7 +62,7 @@ function Footer() {
           </ul>
         </div>
       </div>
-      <div className='flex '>Copyrite 2023</div>
+      <div className='flex '>Copyrite 2023 Otofast</div>
     </div>
   );
 }
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
 
   return (
     <div className="flex justify-between items-center h-24 max-w-[1240px] mx-auto text-white">
-      <h1 className="w-full text-3xl font-bold text-[#00df9a]">StartHub</h1>
+      <h1 className="w-full text-3xl font-bold text-[#00df9a]">Otofast</h1>
       <ul className="hidden md:flex">
         <li className="p-4">Home</li>
         <li className="p-4">Company</li>

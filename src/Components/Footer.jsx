eb6219c4d30083358134a9ddf07feff9f0ccd3c3
marksikaundi <emmanuelmark117@gmail.com>
2023-03-17 23:59:42 +0200

    updates

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -12,10 +12,10 @@ function Footer() {
       <div>
         <h1 className="w-full text-3xl font-bold text-[#00df9a]">Otofast</h1>
         <p className="py-4">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident,
-          velit, ea dolor mollitia id, expedita animi repellendus ducimus eos
-          quidem iste? Soluta omnis nobis illo optio pariatur assumenda est
-          quisquam?
+          Otofast is a software program designed for hearing care professionals
+          such as audiologists and hearing aid specialists. It is a
+          comprehensive solution that streamlines hearing assessment and hearing
+          aid fitting processes.
         </p>
         <div className="flex justify-between md:w-[78%] my-6">
           <FaFacebookSquare size={30} />
@@ -26,43 +26,43 @@ function Footer() {
       </div>
       <div className="lg:col-span-2 mt-6 flex justify-between">
         <div>
-            <h6 className='font-medium text-gray-400'>Solution</h6>
+          <h6 className="font-medium text-gray-400">Solution</h6>
           <ul>
-            <li className='py-2 text-sm'>Analytics</li>
-            <li className='py-2 text-sm'>Commerce</li>
-            <li className='py-2 text-sm'>Marketing</li>
-            <li className='py-2 text-sm'>Insight</li>
+            <li className="py-2 text-sm">Analytics</li>
+            <li className="py-2 text-sm">Commerce</li>
+            <li className="py-2 text-sm">Marketing</li>
+            <li className="py-2 text-sm">Insight</li>
           </ul>
         </div>
         <div>
-            <h6 className='font-medium text-gray-400'>Support</h6>
+          <h6 className="font-medium text-gray-400">Support</h6>
           <ul>
-            <li className='py-2 text-sm'>Pricing</li>
-            <li className='py-2 text-sm'>Documentation</li>
-            <li className='py-2 text-sm'>Guides</li>
-            <li className='py-2 text-sm'>API status</li>
+            <li className="py-2 text-sm">Pricing</li>
+            <li className="py-2 text-sm">Documentation</li>
+            <li className="py-2 text-sm">Guides</li>
+            <li className="py-2 text-sm">API status</li>
           </ul>
         </div>
         <div>
-            <h6 className='font-medium text-gray-400'>Company</h6>
+          <h6 className="font-medium text-gray-400">Company</h6>
           <ul>
-            <li className='py-2 text-sm'>About</li>
-            <li className='py-2 text-sm'>Blog</li>
-            <li className='py-2 text-sm'>Forum</li>
-            <li className='py-2 text-sm'>Careers</li>
+            <li className="py-2 text-sm">About</li>
+            <li className="py-2 text-sm">Blog</li>
+            <li className="py-2 text-sm">Forum</li>
+            <li className="py-2 text-sm">Careers</li>
           </ul>
         </div>
         <div>
-            <h6 className='font-medium text-gray-400'>Legal</h6>
+          <h6 className="font-medium text-gray-400">Legal</h6>
           <ul>
-            <li className='py-2 text-sm'>Claims</li>
-            <li className='py-2 text-sm'>Policy</li>
-            <li className='py-2 text-sm'>Terms</li>
-            <li className='py-2 text-sm'>Courses</li>
+            <li className="py-2 text-sm">Claims</li>
+            <li className="py-2 text-sm">Policy</li>
+            <li className="py-2 text-sm">Terms</li>
+            <li className="py-2 text-sm">Courses</li>
           </ul>
         </div>
       </div>
-      <div className='flex '>Copyrite 2023 Otofast</div>
+      <div className="flex ">Copyrite 2023 Otofast</div>
     </div>
   );
 }

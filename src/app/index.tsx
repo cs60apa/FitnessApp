1c254f5bf9feab1d440ab64cff60cacc7283f64b
marksikaundi <emmanuelmark117@gmail.com>
2024-03-10 17:36:48 +0200

    rm some unwanted

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,16 +1,9 @@
-import React from "react";
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import { Link } from "expo-router";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hello There Team</Text>
-
-      <Link href="/dashboard">Go to Dashboard</Link>
-
-      <StatusBar style="auto" />
     </View>
   );
 }

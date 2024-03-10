e5b4d17493d457908ab692f48291d8ad42f0593f
marksikaundi <emmanuelmark117@gmail.com>
2024-03-10 17:58:31 +0200

    One food items done

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,26 @@
 import { StyleSheet, Text, View } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello There Team</Text>
+      {/* Food Items View */}
+      <View
+        style={{
+          backgroundColor: "gainsboro",
+          padding: 10,
+          borderRadius: 5,
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <View style={{ flex: 1, gap: 5 }}>
+          <Text style={{ fontWeight: "bold", fontSize: 16 }}>Pizza</Text>
+          <Text style={{ color: "dimgray" }}>350 cal, Dominos</Text>
+        </View>
+        <AntDesign name="pluscircleo" size={24} color="royalblue" />
+      </View>
     </View>
   );
 }
@@ -12,7 +29,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
     justifyContent: "center",
+    padding: 10,
   },
 });

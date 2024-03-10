05363e3d6e6eeb0dad5c1af515390c1843f985b7
marksikaundi <emmanuelmark117@gmail.com>
2024-03-10 17:34:40 +0200

    created layouts

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,15 @@
+import React from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { Link } from "expo-router";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello There Team</Text>
+
+      <Link href="/dashboard">Go to Dashboard</Link>
+
       <StatusBar style="auto" />
     </View>
   );

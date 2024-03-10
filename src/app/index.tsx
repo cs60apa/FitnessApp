b250d98350a8aff288e16b311e7cbc289b248ba7
marksikaundi <emmanuelmark117@gmail.com>
2024-03-11 00:19:08 +0200

    Add search functionality and update styling

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,37 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, FlatList, TextInput, Button } from "react-native";
 import FoodListItem from "../components/FoodListItem";
+import { useState } from "react";
+
+const foodListItems = [
+  { label: "Pizza", cal: 75, brand: "Dominos" },
+  { label: "Apple", cal: 50, brand: "Generic" },
+  { label: "Orange", cal: 40, brand: "Gastric" },
+];
 
 export default function App() {
+  const [search, setSearch] = useState("");
+
+  const performSearch = () => {
+    console.log("Search for: ", search);
+
+    setSearch("");
+  };
+
   return (
     <View style={styles.container}>
-      {/* Food Items View */}
-      <FoodListItem item={{ label: "Pizza", cal: 75, brand: "Dominos" }} />
-      <FoodListItem item={{ label: "Apple", cal: 50, brand: "Generic" }} />
+      <TextInput
+        value={search}
+        onChangeText={setSearch}
+        placeholder="Search for food"
+        style={styles.input}
+      />
+      {search && <Button  title="Search" onPress={performSearch}  />}
+
+      <FlatList
+        data={foodListItems}
+        renderItem={({ item }) => <FoodListItem item={item} />}
+        contentContainerStyle={{ gap: 5 }}
+      />
     </View>
   );
 }
@@ -15,8 +40,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    justifyContent: "center",
     padding: 10,
-    gap: 5,
+    gap: 10,
+  },
+  input: {
+    backgroundColor: "#f2f2f2",
+    padding: 10,
+    borderRadius: 20,
   },
 });
